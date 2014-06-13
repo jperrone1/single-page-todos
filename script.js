17ab8aac0ae6a97594ef28ef7a9bb0d863ddead8
Jeffrey Perrone <jperrone@well.com>
2014-06-13 10:52:19 -0700

    Cleaned up code.

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
 "use strict"; 
 
-// console.log('hello');
-
 $(function() { 
-
 
   $('#addButton').click(function(e){
     e.preventDefault();
     var toDo = $('#toDoItem').val();
-    // console.log(toDo);
-
 
     $( '#tasks' ).append ("<div class='task'>" + toDo + "<img src='assets/trashcan_delete.png'></div>");
     $('#toDoItem').val(''); 
   });
 
-  // $( '.task' ).click(function(e){
-  //   $(this).toggleClass("toDoDone");
-  // });
 
-    $( '#tasks' ).delegate('.task', 'click', function(e){
+  $( '#tasks' ).delegate('.task', 'click', function(e){
     $(this).toggleClass("toDoDone");
   });
 
-    $( '#tasks' ).delegate('.task img', 'click', function(e){
+  $( '#tasks' ).delegate('.task img', 'click', function(e){
     $(this).closest('.task').remove();
   });
 
-})
+});
